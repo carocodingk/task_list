f31d57487f6974748d7880c56ad64e215034c587
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-04-01 18:28:33 -0400

    Added submit button, Added new tasks to display on list

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,37 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import {useState} from "react";
+
+const tasks = [
+  "Being alive",
+  "Eating noodles",
+  "Taking a shower",
+  "Cleaning the bathroom",
+];
 
 export default function Home() {
+  const [newTask, setNewTask] = useState("wacky");
+  const [changeList, setChangeList] = useState(tasks);
+  console.log(newTask. changeList)
   return (
     <div id="listSquare"> 
       <h1>To do list</h1> 
       <div>
         <ul>
-          <li>Being alive</li>
-          <li>Eating noodles</li>
-          <li>Taking a shower</li>
-          <li>Cleaning the bathroom</li>
+          {changeList.map((x) => (
+            <li>{x}</li>
+          ))}
         </ul>
       </div>
       <div>
-        <form>
-          <input type="text" />
-        </form>
+        <div>
+          <label for="task"></label>
+          <input type="text" id="task" name="task" value={newTask} onChange={(e)=>{setNewTask(e.target.value)}}/>
+          <input type='submit' value="Add" onClick={()=>{setChangeList([...changeList, newTask])}}/>
+        </div>
       </div>
     </div>
   );
 }
+
