ebbbca2fafb303270939f00a29f4fdef0dfee369
CaroCodingK <130165356+carocodingk@users.noreply.github.com>
2024-04-03 19:41:09 -0400

    Added save and cancel functionality when editing an existing task

--- src/app/page.js
+++ src/app/page.js
@@ -13,25 +13,44 @@ const tasks = [
 export default function Home() {
   const [newTask, setNewTask] = useState("wacky");
   const [changeList, setChangeList] = useState(tasks);
+  const [selectedItemIdx, setSelectedItemIdx] = useState(null);
   // console.log(newTask. changeList)
 
   const deletingTask = (idx) => {
     const deleteTasks = changeList.toSpliced(idx, 1)
     setChangeList(deleteTasks)
   }
+  console.log(selectedItemIdx)
+
+  const editingTask = (idx) => {
+    const editedTask = changeList.toSpliced(idx, 1, newTask)
+    setChangeList(editedTask)
+    setSelectedItemIdx(null)
+    console.log(editedTask)
+  }
+
 
   return (
     <div id="listSquare"> 
       <h1>To do list</h1> 
       <div>
         <ul>
-          {changeList.map((x, idx) => (
+          {changeList.map((task, idx) => (
             <li>
-              <div>
-                {x}
-                <input type="button" value="edit" />
-                <input type="button" value="delete" onClick={()=>deletingTask(idx)} />
-              </div>
+              {idx === selectedItemIdx 
+                ? <div>
+                    <input type="text" value={changeList[idx]} onChange={(e)=>{setNewTask(e.target.value)}} />
+                    <input type="button" value="save" onClick={()=>editingTask(idx)} />
+                    <input type="button" value="cancel" onClick={()=>{setSelectedItemIdx(null)}}/>
+                  </div>
+                : (
+                  <div>
+                    {task}
+                    <input type="button" value="edit" onClick={()=>setSelectedItemIdx(idx)}/>
+                    <input type="button" value="delete" onClick={()=>deletingTask(idx)} />
+                  </div>
+                )
+              }
             </li>
           ))}
         </ul>
